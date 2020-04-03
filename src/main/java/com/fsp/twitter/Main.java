96f66899667d06618af535a78ae130f3a0292cac
hamid <sahlolbey@gmail.com>
2020-04-03 16:24:33 +0430

    initialliy added

---
+++ src/main/java/com/fsp/twitter/Main.java
@@ -0,0 +1,10 @@
+package com.fsp.twitter;
+
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+}
